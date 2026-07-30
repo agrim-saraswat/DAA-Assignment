a578a5426d18fa379ba572ad66f1f8a5c09c9ffd
agrim-saraswat <agrimsaraswat12@gmail.com>
2026-07-30 11:14:14 +0530

    Time: 0 ms (100%), Space: 43.7 MB (80.32%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+    int l = 0, r = m * n - 1;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        int midVal = matrix[mid / n][mid % n];
+        if (midVal == target) return true;
+        if (midVal < target) l = mid + 1;
+        else r = mid - 1;
+    }
+    return false;
+    }
+}
